a9e1f321a58361ff1f12faea5868ff80d3b7eb3c
gy2006 <32008001@qq.com>
2020-05-23 12:05:46 +0200

    add latest tag for plugin runtime image

--- plugin-runtime/build.sh
+++ plugin-runtime/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-img=flowci/plugin-runtime:1.0
-docker build -t ${img} .
-echo "Image ${img} has been built"
+img=flowci/plugin-runtime
+ver=1.0
+
+docker build -t ${img}:${ver} -t ${img}:latest .
+echo "Image ${img}:${ver} has been built"
